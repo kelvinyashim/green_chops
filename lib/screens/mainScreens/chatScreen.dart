93d5f00a0f00626924e2192133aaf8093966d862
KELVIN <kelvinyashim24@gmail.com>
2024-01-10 01:59:21 +0100

    added message data model

--- lib/screens/mainScreens/chatScreen.dart
+++ lib/screens/mainScreens/chatScreen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:green_chops/screens/chat/chatRoom.dart';
+import 'package:green_chops/screens/form/login.dart';
 import 'package:green_chops/services/chat/chat.dart';
 import 'package:green_chops/widgets/userTile.dart';
 
@@ -9,53 +10,44 @@ class ChatScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Chat'),
-        centerTitle: true,
-      ),
-      body: buildUserList(),
-    );
-  }
+      
+        appBar: AppBar(
+          automaticallyImplyLeading: false,
+          title: Text('Chat'),
+          centerTitle: true,
+        ),
+        body: StreamBuilder(
+          stream: chatService.getUsers(),
+          builder: (context, snapshot) {
+            if (!snapshot.hasData) {
+              return Center(child: CircularProgressIndicator());
+            }
+            final users = snapshot.data;
 
-  Widget buildUserList() {
-    return StreamBuilder<List<Map<String, dynamic>>>(
-      stream: chatService.getuserStream(),
-      builder: (context, snapshot) {
-        //error
-        if (snapshot.hasError) {
-          return Center(child: Text("${snapshot.error}"));
-        }
-        //loading
-        if (snapshot.connectionState == ConnectionState.waiting) {
-          return const Center(
-            child: Text("Loading"),
-          );
-        }
-        final List<Map<String, dynamic>> userList = snapshot.data!;
-        print("User List Length: ${userList.length}");
-        return ListView(
-            children: userList
-                .map<Widget>((userData) => buildUserListItem(userData, context))
-                .toList());
-      },
-    );
-  }
-
-//this is to build a tile for each user
-//minus the curr user
-  Widget buildUserListItem(
-      Map<String, dynamic> userData, BuildContext context) {
-    return UserTile(
-      email: userData['email'],
-      onTap: () {
-        Navigator.of(context).push(MaterialPageRoute(
-          builder: (context) {
-            return ChatPage(
-              receiverEmail: userData["email"],
+            return ListView.builder(
+              itemCount: users!.length,
+              itemBuilder: (context, index) {
+                if(users[index]["email"] != firebase.currentUser!.email){
+                  return ListTile(
+                  title: Text(users[index]["email"]),
+                  textColor: Colors.black,
+                  onTap: () {
+                    Navigator.of(context).push(MaterialPageRoute(
+                      builder: (context) {
+                        return ChatPage(
+                          receiverEmail: users[index]["email"],
+                          
+                          );
+                      },
+                    ));
+                  },
+                );
+                }
+                return Container();
+                
+              },
             );
           },
         ));
-      },
-    );
   }
 }
--- lib/services/chat/chat.dart
+++ lib/services/chat/chat.dart
@@ -1,21 +1,62 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:green_chops/model/chat/message.dart';
+import 'package:green_chops/screens/form/login.dart';
 
 class ChatService {
   //first we get the instance of firestore
   final firestore = FirebaseFirestore.instance;
 
   //now we get the user stream to display the users
-  Stream<List<Map<String, dynamic>>> getuserStream() {
-    return firestore.collection('user').snapshots().map((snapshot) {
+  Stream<List<Map<String, dynamic>>> getUsers() {
+    return firestore.collection("user").snapshots().map((snapshot) {
       return snapshot.docs.map((doc) {
         final user = doc.data();
         return user;
       }).toList();
     });
   }
-  //the first line allows us to iterate over the snapshots
-  //the snapshot listen for changes and the map is used to modify the data within the sshot
-  //the snapshot parameter gives us an instance of a single data within the stream
-  //
+
+  //send message
+
+  Future<void> sendMessage(String receiverId, message) async {
+    //get current user info
+    final currUserId = firebase.currentUser!.uid;
+    final currUserEmail = firebase.currentUser!.email;
+    final timestamp = Timestamp.now();
+
+    //send a message
+    final newMessage = Message(
+        senderId: currUserId,
+        senderEmail: currUserEmail!,
+        receiverId: receiverId,
+        message: message,
+        timestamp: timestamp);
+
+    //construct chat room id for the two users
+    List<String> ids = [currUserId, receiverId];
+    ids.sort();
+    String chatroomId = ids.join('-');
+
+    //add msg to database
+    await firestore
+        .collection('chatRoom')
+        .doc(chatroomId)
+        .collection("messages")
+        .add(newMessage.toMap());
+  }
+
+  //get message
+  Stream<QuerySnapshot> getMessages(String userId, otherId) {
+    //construct a chatroom id for the two users
+    List<String> ids = [userId, otherId];
+    ids.sort();
+    String chatroomId = ids.join('-');
+
+    return firestore
+        .collection("chatRoom")
+        .doc(chatroomId)
+        .collection("messages")
+        .orderBy("timestamp", descending: false)
+        .snapshots();
+  }
 }
-//Now we build a list of the users so we can display them
